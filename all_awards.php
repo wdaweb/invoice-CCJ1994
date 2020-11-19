4d12b68fd87c9df6ffd249cc52527c970f8f958b
ccj1994 <a0changj2@gmail.com>
2020-11-19 10:26:51 +0800

    完成單期發票對獎

--- all_awards.php
+++ all_awards.php
@@ -18,6 +18,7 @@ echo $period_str[$_GET['period']]."的發票";
 $sql="select * from invoices where left(date,4)='{$_GET['year']}' && period='{$_GET['period']}' order by date desc";
 $invoices=$pdo->query($sql)->fetchALL();
 
+
 echo "<br>".count($invoices)."筆<br>";
 // echo "<pre>";
 // print_r($invoices);
@@ -38,6 +39,79 @@ if(!empty($award_numbers)){
 
 
 // 開始對獎
+$all_res=-1;
+foreach($invoices as $inv){
+  
+  //對獎程式
+  $number=$inv['number'];
+
+// echo "<pre>";
+// print_r($invoice);
+// echo "<pre>";
+
+// 找出獎號
+// 1.確認期別->目前發票的日期作分析
+// 2.得捯期別資料後->撈出該期的開獎號碼
+
+$date=$inv['date'];
+// explode('-',$date)取得日期資料，陣列的第二個元素就是月份
+// 月份就可以推算期數，有了期數及年份就可以找到開獎的號碼
+// $arry=explode('-',$date)
+// $month=$arry[1]
+// $period=ceil($month/2)
+$year=explode('-',$date)[0];
+$period=ceil(explode('-',$date)[1]/2);
+
+
+foreach($award_numbers as $award){
+    switch($award['type']){
+        case 1:
+            if($award['number']==$number){
+                echo "<br>號碼=".$number."<br>";
+                echo "中了特別獎";
+                $all_res=1;
+            }
+        break;
+        case 2:
+            if($award['number']==$number){
+                echo "<br>號碼=".$number."<br>";
+                echo "中了特獎";
+                $all_res=1;
+            }
+        break;
+        case 3:
+            $res=-1;
+            for($i=5;$i>=0;$i--){
+                $target=mb_substr($award['number'],$i,(8-$i),'utf8');
+                $mynumber=mb_substr($number,$i,(8-$i),'utf8');
+
+                if($target==$mynumber){
+                    
+                    $res=$i;
+                }else{
+                break;
+                }
+            }
+            if($res!=-1){
+
+                echo "<br>號碼=".$number."<br>";
+                echo "中了{$awardStr[$res]}獎";
+                $all_res=1;
+            }
+        break;
+        case 4:
+            if($award['number']==mb_substr($number,5,3,'utf8')){
+                echo "<br>號碼=".$number."<br>";
+                echo "中了增六獎";
+                $all_res=1;
+            }
+        break;
+    }
+}
+}
+  if($all_res==-1){
+    echo "很可惜都沒中";
+  }
 
 
 
